3f1d2233c3ae707be3192825e20b64d512ec66fa
Vanessa Henson <vanessahenson@Vanessas-MacBook-Air.local>
2017-10-29 10:02:03 -0600

    have the lambda function returning the correct mimetype of basestring and have it working with the S3 topic

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,28 @@
 import boto3
-import StringIO
+from botocore.client import Config
 import zipfile
-import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('vanessahenson.ninja')
-build_bucket = s3.Bucket('build-output.vanessahenson')
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:040464660539:deployportfollioTopic')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfolliobuild.zip', portfolio_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        build_bucket = s3.Bucket('build-output.vanessahenson')
+        portfolio_bucket = s3.Bucket('vanessahenson.ninja')
 
+        # On Windows, this will need to be a different location than /tmp
+        build_bucket.download_file('portfolliobuild.zip', '/tmp/portfolio.zip')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, 
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile('/tmp/portfolio.zip') as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':  'basestring'})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job well done'
+        topic.publish(Subject="Portfollio Deployed", Message="Portfollio Successfully Deployed")
+
+    except:
+        topic.publish(Subject="Portfollio Failed", Message="Portfollio Was NOT deployed sucessfully")
+        raise
