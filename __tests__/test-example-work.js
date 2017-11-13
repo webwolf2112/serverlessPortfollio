3bc66b11a7b4b48f4beec7f38f764b5c4baec93a
Vanessa Henson <vanessahenson@Vanessas-MacBook-Air.local>
2017-11-12 18:42:28 -0700

    got the react components rendering and started on the tests

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExampleWork from '../js/example-work';
+import { configure } from 'enzyme';
 
 //jest is running these tests
 
+const myWork = [
+  {
+    'title': "My Work 1",
+    'image': {
+      'src': "images/example1.png",
+      'desc': "example screenshot of a project involving code",
+      'comment': "This is some example work by some people"
+    }
+  }, {
+    'title': "My Work 2",
+    'image': {
+      'src': "images/example2.png",
+      'desc': "example screenshot of a project involving cats",
+      'comment': "This is some example work by some cats"
+    }
+  }];
+
 describe('ExampleWork component', () => {
-	it("Should be a 'section' element")
+	let component = shallow(<ExampleWork work={myWork}/>);
+	it("Should be a 'section' element", () => {
+		console.log(component.debug());
+	});
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-import 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log('Loaded React-dom'); 
-console.log('webpack works');
+const myWork = [
+  {
+    'title': "My Work 1",
+    'image': {
+      'src': "images/example1.png",
+      'desc': "example screenshot of a project involving code",
+      'comment': "This is some example work by some people"
+    }
+  }, {
+    'title': "My Work 2",
+    'image': {
+      'src': "images/example2.png",
+      'desc': "example screenshot of a project involving cats",
+      'comment': "This is some example work by some cats"
+    }
+  }, {
+    'title': "My Work 3",
+    'image': {
+      'src': "images/example3.png",
+      'desc': "example screenshot of a project involving other stuff",
+      'comment': `This is a mulit line Comment.
+                So I have used the back tick`
+    }
+  }
+]
+
+ReactDom.render(<ExampleWork work={myWork}/>, document.getElementById('exampleWork'));
